d6afe59faf0d92c2a88444cff5687dace68e076a
samcarter <sam.carter8@aol.com>
2023-11-27 23:47:50 +0100

    showing progress while converting pdf to video

--- example-scene/convert2video.sh
+++ example-scene/convert2video.sh
@@ -2,7 +2,7 @@
 rm Example.mp4
 
 # convert to png images
-pdftoppm -png -r 240 Example.pdf Example
+pdftoppm -png -r 240 -progress Example.pdf Example
 
 # convert to video
 ffmpeg  -ss 00:00:00 -i Example-%03d.png -ss 00:00:10 -i Music.m4a -shortest Example_raw.mp4
