6dfa168f6814bdf28259885a72624a7140144e1c
samcarter <sam.carter8@aol.com>
2023-12-13 10:44:47 +0100

    clean up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The Great TikZlings Christmas Extravaganza 2023
 
-This repository contains the souce code of the Great TikZlings Extravanganza 2023, created by ...
+This repository contains the souce code of the Great TikZlings Extravanganza 2023, brought to you by [Ulrike](https://github.com/u-fischer) & [Gert Fischer](https://github.com/g-fischer), [Carla Maggi](https://github.com/CarLaTeX), [Paulo Cereda](https://github.com/cereda) and [samcarter](https://github.com/samcarter).
 
 The full video is available from ...
 
--- carlatex/Tree/convert2video.sh
+++ carlatex/Tree/convert2video.sh
@@ -5,7 +5,7 @@ rm Xmas_duck_tree.mp4
 pdftoppm -png -r 240 -progress Xmas_duck_tree_repeat.pdf Xmas_duck_tree
 
 # convert to video
-ffmpeg  -ss 00:00:00 -i Xmas_duck_tree-%03d.png -ss 00:00:05 -i Rockin_Around_The_Christmas_Tree-Brenda_Lee-O1x-hOOrl5E.m4a -shortest Xmas_duck_tree_raw.mp4
+ffmpeg  -ss 00:00:00 -i Xmas_duck_tree-%03d.png -ss 00:00:06 -i Rockin_Around_The_Christmas_Tree-Brenda_Lee-O1x-hOOrl5E.m4a -shortest Xmas_duck_tree_raw.mp4
 
 # repair video
 HandBrakeCLI --crop 0:0:0:0  -i Xmas_duck_tree_raw.mp4 -o Xmas_duck_tree.mp4
